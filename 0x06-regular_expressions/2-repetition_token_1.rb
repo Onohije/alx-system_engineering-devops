43842a7df338910d99f1a661e9ffe90d4b8c9439
Onohije <igbadumeonoh@gmail.com>
2023-08-01 13:17:03 -0700

    regular expressions

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Regular expression that matches a given pattern in the input
+puts ARGV[0].scan(/hb?tn/).join
---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# A regular expression that finds all matches  of a given pattern
+puts ARGV[0].scan(/hbt+n/).join
